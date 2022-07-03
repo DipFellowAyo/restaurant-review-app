27630f4aeaeadad9109b2495e69fd1c5e9a34c33
DipFellowAyo <103995759+DipFellowAyo@users.noreply.github.com>
2022-07-03 14:10:10 +0100

    Updates

---
+++ precache-manifest.f2ca8808236523abc56559a480f677e6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d64af19b2fba611e4a1c",
+    "url": "/restaurant-review-app/static/js/main.d64af19b.chunk.js"
+  },
+  {
+    "revision": "4b3d5599484fe9b4418f",
+    "url": "/restaurant-review-app/static/js/1.4b3d5599.chunk.js"
+  },
+  {
+    "revision": "b18ab81ac3dc915d8ed4",
+    "url": "/restaurant-review-app/static/js/runtime~main.b18ab81a.js"
+  },
+  {
+    "revision": "7b1f5a34b564eaf294b4129ddbcea9fc",
+    "url": "/restaurant-review-app/index.html"
+  }
+];
